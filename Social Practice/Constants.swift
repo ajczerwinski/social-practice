9d4da02b0267fa49536402194ca8be44ffff0417
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-07-07 20:49:47 -0700

    complete sign-in-ui custom classes

---
+++ Social Practice/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social Practice
+//
+//  Created by Allen Czerwinski on 7/7/17.
+//  Copyright © 2017 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
