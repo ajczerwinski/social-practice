7f2f0d8f2158cd84eab4a26c38f757ec73dc0030
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-07-10 15:49:55 -0700

    complete FB and Firebase auth setup

--- Social Practice/SignInVC.swift
+++ Social Practice/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,54 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        /*facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler: {(result, error) in
+            
+            if error != nil {
+                print("AllenError: Login error \(error!)")
+            } else if result?.isCancelled == true {
+                print("AllenError: User canceled FB authentication")
+            } else {
+                print("AllenSuccess: successfully authenticated with FB")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+            }
+            
+        })*/
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                print("AllenError: Login error \(error!)")
+            } else if result?.isCancelled == true {
+                print("AllenError: User canceled FB authentication")
+            } else {
+                print("AllenSuccess: successfully authenticated with FB")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil {
+                print("AllenError: Unable to authenticate with Firebase \(error!)")
+            } else {
+                print("AllenSuccess: Successfully authenticated with Firebase")
+            }
+            
+        })
+        
+    }
 
 }
 
