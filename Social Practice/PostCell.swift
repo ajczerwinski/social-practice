f0451d79d5958d9e37847fd0a45240fedf0909ee
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-07-17 17:19:19 -0700

    complete Firebase display

--- Social Practice/PostCell.swift
+++ Social Practice/PostCell.swift
@@ -15,6 +15,8 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -27,5 +29,11 @@ class PostCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
+    }
 
 }
