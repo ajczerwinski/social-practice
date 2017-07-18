ce230ab43836d3f6743c7310e14ffb7d28a2113e
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-07-18 16:36:10 -0700

    implement photopicker

--- Social Practice/CircleView.swift
+++ Social Practice/CircleView.swift
@@ -20,6 +20,8 @@ class CircleView: UIImageView {
     override func layoutSubviews() {
         super.layoutSubviews()
         layer.cornerRadius = self.frame.width / 2
+        clipsToBounds = true
+        
     }
 
 }
